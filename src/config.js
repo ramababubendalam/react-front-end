e21f0192419a15294fe4763e287d889735e4d2ba
ramababubendalam <haib.ram@gmail.com>
2022-08-14 22:16:37 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
   // Backend config
 
   BackendUrl:{
-    API_URL: "https://38a7xrtj2h.execute-api.eu-west-2.amazonaws.com/dev01",
+    API_URL: "https://76icaj1r1k.execute-api.eu-west-2.amazonaws.com/dev01/",
     TIME_URL: "http://api.timezonedb.com/v2.1/convert-time-zone?key=M3QAQGMSCU9K&format=json&from=UTC&to=America/Jamaica&time="
   },
 };
